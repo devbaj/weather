b69701f3cdc66271c7275a5556235f6c6e8b3f30
Brett Jory <bjory7@gmail.com>
2019-03-28 12:11:06 -0700

    functionality complete: page access openweather API and pulls city name and temp, adds appropriate content to html, replaces itself on subsequent rather than appending itself

---
+++ main.js
@@ -0,0 +1,23 @@
+function kelvinToCelsius(x){
+    return (x-273);
+}
+function celsiusToFahr(x){
+    return (x * (9/5)) + 32;
+}
+
+$(document).ready(function() {
+
+    $('form').submit(function() {
+        console.log("submitted")
+        var url = "http://api.openweathermap.org/data/2.5/weather?q=" + $('input[type=text][name=city]').val() + "&&appid=e6c2cdf246401ea36c85ef3da3a5cc0a";
+        $.get(url, function(res) {
+            var htmlStr = "<h2>" + res.name + "</h2>";
+            htmlStr += "<h3>Temperature: " + 
+                Math.trunc(celsiusToFahr(kelvinToCelsius(res.main.temp)))
+                + " degrees Fahrenheit</p>";
+            console.log(htmlStr);
+            $('.output').html(htmlStr);
+        }, "json");
+        return false;
+    })
+});
