5e967bf3a4adbb8503706ac920677bf755da9792
Brett Jory <bjory7@gmail.com>
2019-03-28 12:14:37 -0700

    code cleaned up, page functional

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
         var url = "http://api.openweathermap.org/data/2.5/weather?q=" + $('input[type=text][name=city]').val() + "&&appid=e6c2cdf246401ea36c85ef3da3a5cc0a";
         $.get(url, function(res) {
             var htmlStr = "<h2>" + res.name + "</h2>";
-            htmlStr += "<h3>Temperature: " + 
+            htmlStr += "<p>Temperature: " + 
                 Math.trunc(celsiusToFahr(kelvinToCelsius(res.main.temp)))
                 + " degrees Fahrenheit</p>";
             console.log(htmlStr);
